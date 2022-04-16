e04949d82c2d232f4a5b2d7f35c360f4e558b048
BSaenz12 <61446016+BSaenz12@users.noreply.github.com>
2022-04-16 14:26:06 -0500

    Client pages 04162022 (#5)
    * Added client properties.
    
    * Updated property data type.

--- QuickFixers.Data/Models/Clients.cs
+++ QuickFixers.Data/Models/Clients.cs
@@ -8,6 +8,11 @@ namespace QuickFixers.Data.Models
 {
    public class Clients
     {
-
+        public int ClientID { get; set; }
+        public int UserID { get; set; }
+        public string Name { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Address { get; set; }
+        public int ZipCode { get; set; }
     }
 }
