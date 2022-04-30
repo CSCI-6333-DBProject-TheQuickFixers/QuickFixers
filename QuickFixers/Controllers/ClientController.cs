13115b2c2d3bf5c0396c9edacb314e4d11efaab5
BSaenz12 <Brunosaenz8@gmail.com>
2022-04-29 21:54:29 -0500

    Added ClientController.

---
+++ QuickFixers/Controllers/ClientController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace QuickFixers.Controllers
+{
+    public class ClientController : Controller
+    {
+        // GET: Client
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Services()
+        {
+            return View();
+        }
+
+        public ActionResult ScheduledServices()
+        {
+            return View();
+        }
+
+        public ActionResult PastServices()
+        {
+            return View();
+        }
+
+    }
+}
