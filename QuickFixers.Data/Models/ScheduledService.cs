7f41b622d68734754449afc9ec4662ed9f796f79
BSaenz12 <Brunosaenz8@gmail.com>
2022-05-07 14:55:50 -0500

    Added new properties.

--- QuickFixers.Data/Models/ScheduledService.cs
+++ QuickFixers.Data/Models/ScheduledService.cs
@@ -10,9 +10,13 @@ namespace QuickFixers.Data.Models
     {
         public int scheduledServiceID { get; set; }
         public int serviceProviderID { get; set; }
+        public string serviceProviderName { get; set; }
         public int clientID { get; set; }
+        public string clientName { get; set; }
         public int servicesOfferedID { get; set; }
+        public string serviceTypeName { get; set; }
         public DateTime serviceDate { get; set; }
         public string serviceAddress { get; set; }
+        public double serviceFee { get; set; }
     }
 }
