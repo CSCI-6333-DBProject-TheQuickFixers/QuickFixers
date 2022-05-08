4e91354f001872c5ad54892f162d4df961fefa6c
BSaenz12 <Brunosaenz8@gmail.com>
2022-05-08 04:35:58 -0500

    Updated controller

--- QuickFixers/Controllers/ClientController.cs
+++ QuickFixers/Controllers/ClientController.cs
@@ -1,6 +1,10 @@
-﻿using QuickFixers.Data.Services;
+﻿using QuickFixers.Data.DataBase;
+using QuickFixers.Data.Models;
+using QuickFixers.Data.Services;
+using QuickFixers.Models;
 using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -24,7 +28,8 @@ namespace QuickFixers.Controllers
 
         public ActionResult Services()
         {
-            return View();
+
+            return View(new ServiceViewModel());
         }
 
         public ActionResult ScheduledServices()
@@ -38,6 +43,59 @@ namespace QuickFixers.Controllers
         {
             var model = db.GetAllPast();
             return View(model);
+        }
+
+        public ActionResult SelectService(List<Service> selectedServices)
+        {
+            var model = selectedServices;
+            return View(model);
+        }
+
+        [HttpPost]
+        public ActionResult Services(ServiceViewModel serviceViewModelPost)
+        {
+            if (ModelState.IsValid)
+            {
+                Service searchService = new Service
+                {
+                    ServiceTypeID = serviceViewModelPost.ServiceTypeID,
+                    ServiceProviderName = serviceViewModelPost.ServiceProviderName,
+                    ZipCode = serviceViewModelPost.ZipCode,
+                    PreferredDistance = serviceViewModelPost.PreferredDistance
+                };
+
+                // searchService      
+                DayOfWeek daytest = serviceViewModelPost.StartDate.DayOfWeek;
+                String serviceTime = serviceViewModelPost.StartDate.ToString("HH:mm:ss");
+                Dictionary<string, string> spParameters = new Dictionary<string, string>();
+
+                // Add parameter names and values to dictionary
+                spParameters.Add("@ServiceTypeID", serviceViewModelPost.ServiceTypeID.ToString());
+                spParameters.Add("@ServiceProviderName", serviceViewModelPost.ServiceProviderName);
+                spParameters.Add("@ZipCode", serviceViewModelPost.ZipCode.ToString());
+                spParameters.Add("@PreferredDistance", serviceViewModelPost.PreferredDistance.ToString());
+                spParameters.Add("@DayOfTheWeek", daytest.ToString());
+                spParameters.Add("@ServiceTime", serviceTime);
+
+
+                DataTable selectedServices = DatabaseSelections.Select("quickFixers.selectServices", spParameters);
+
+                if ((selectedServices != null) & (selectedServices.Rows.Count > 0))
+                {
+                    List<Service> servicesFound = DatabaseSelections.ConvertToList<Service>(selectedServices);
+
+                    return View(servicesFound);
+                }
+                else
+                {
+                    ModelState.AddModelError("No Services that meet that criteria.", "Try other data.");
+                    return View();
+                }
+            }
+            else
+            {
+                return RedirectToAction("Index", "Login");
+            }
         }
 
     }
