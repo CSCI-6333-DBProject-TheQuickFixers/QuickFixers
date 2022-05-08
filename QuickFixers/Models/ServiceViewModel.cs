613bd828924d27b0024171265a2b6d355f926741
BSaenz12 <Brunosaenz8@gmail.com>
2022-05-08 04:36:11 -0500

    Updated Models.

---
+++ QuickFixers/Models/ServiceViewModel.cs
@@ -0,0 +1,34 @@
+﻿using QuickFixers.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+
+namespace QuickFixers.Models
+{
+    public class ServiceViewModel
+    {
+        [IntegerValidator(MinValue = 1, MaxValue = 10)]
+        public int ServiceTypeID { get; set; }
+
+        /// <summary>
+        /// This populates are user types to a drop down. 
+        /// </summary>
+        public List<ServiceType> ServiceTypesSelect
+        {
+            get
+            {
+                return ServiceType.GetServiceTypes();
+            }
+        }
+
+        public string ServiceTypeName { get; set; }
+        public int ZipCode { get; set; }
+        public int PreferredDistance { get; set; }
+        public int ServiceProviderID { get; set; }
+        public string ServiceProviderName { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndtDate { get; set; }
+        public DayOfWeek DayOfTheWeek { get; set; }
+
+    }
+}
