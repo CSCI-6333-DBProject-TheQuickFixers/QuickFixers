a5d9699d507dee253ec080a15d54db58d57c7d91
BSaenz12 <Brunosaenz8@gmail.com>
2022-05-08 04:36:48 -0500

    Added new convertolist function

--- QuickFixers.Data/DataBase/DatabaseSelections.cs
+++ QuickFixers.Data/DataBase/DatabaseSelections.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Data;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 using MySql.Data.MySqlClient;
@@ -102,6 +103,27 @@ namespace QuickFixers.Data.DataBase
                 Console.WriteLine(ex.ToString());
             }
             return returnResults;
+        }
+
+        public static List<T> ConvertToList<T>(DataTable dt)
+        {
+            var columnNames = dt.Columns.Cast<DataColumn>()
+                    .Select(c => c.ColumnName)
+                    .ToList();
+            var properties = typeof(T).GetProperties();
+            return dt.AsEnumerable().Select(row =>
+            {
+                var objT = Activator.CreateInstance<T>();
+                foreach (var pro in properties)
+                {
+                    if (columnNames.Contains(pro.Name))
+                    {
+                        PropertyInfo pI = objT.GetType().GetProperty(pro.Name);
+                        pro.SetValue(objT, row[pro.Name] == DBNull.Value ? null : Convert.ChangeType(row[pro.Name], pI.PropertyType));
+                    }
+                }
+                return objT;
+            }).ToList();
         }
     }
 }
