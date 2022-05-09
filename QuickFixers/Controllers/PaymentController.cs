b7d4f7b9b0760f8d1818631952bac29966489e1d
Jody <jodywhitis0407@gmail.com>
2022-05-08 23:58:02 -0500

    Adding Payment Controller/View and Payments Model Classes.

---
+++ QuickFixers/Controllers/PaymentController.cs
@@ -0,0 +1,51 @@
+﻿using QuickFixers.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace QuickFixers.Controllers
+{
+    public class PaymentController : Controller
+    {
+        [HttpGet]
+        public ActionResult Index()
+        {
+            PaymentViewModel paymentViewModelGet = new PaymentViewModel();
+            paymentViewModelGet.AmountDue = 100.50M;
+            return View(paymentViewModelGet);
+        }
+
+
+        [HttpPost]
+        public ActionResult Index(PaymentViewModel paymentViewModelPost)
+        {
+            if (ModelState.IsValid)
+            {
+                Data.Models.Payment newPayment = new Data.Models.Payment();
+                newPayment.ExpirationMonthPayment = paymentViewModelPost.ExpirationMonth;
+                newPayment.ExpirationYearPayment = paymentViewModelPost.ExpirationYear;
+                newPayment.CardNumber = paymentViewModelPost.CardNumber;
+                newPayment.AmountDue = paymentViewModelPost.AmountDue;
+                newPayment.PaymentAmount = paymentViewModelPost.AmountDue;
+
+                Boolean isValidPayment = Data.Models.Payment.MakePayment(newPayment);
+
+                if (isValidPayment)
+                {
+                return RedirectToAction("Index", "Test");
+                }
+                else
+                {
+                    return View("Error");
+                }
+            }
+            else
+            {
+                return View("Error");
+            }
+        }
+
+    }
+}
---
+++ QuickFixers/Models/PaymentViewModel.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace QuickFixers.Models
+{
+    public class PaymentViewModel
+    {
+          
+        [StringLength(maximumLength:16)]
+        public string CardNumber { get; set; }
+
+        public string BankAccount { get; set; }
+
+        public string RoutingNumber { get; set; }
+
+        public int ExpirationMonth { get; set; }
+
+        public int ExpirationYear { get; set; }
+
+        public decimal AmountDue { get; set;  }
+        [Required]     
+        public decimal PaymentAmount { get; set; }
+
+        public List<int> ExpirationMonthList
+        {
+            get
+            {
+                return Data.Models.Payment.ExpirationMonth;
+            }
+        }
+        
+        public List<int> ExpirationYearList
+        {
+            get
+            {
+                return Data.Models.Payment.ExpirationYear;
+            }
+        }
+
+    }
+}
